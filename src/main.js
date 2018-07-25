c741066e0a914360a8e9b1f2e2fd02b154ab3c0a
qinxuecheng <659281944@qq.com>
2018-07-25 19:10:36 +0800

    初始化项目文件

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+
+Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 
+// import news from './components/news.vue'
+// import stars from './components/stars.vue'
+
+// const router = new VueRouter({
+//   routes:[
+//     { path: '/news', component: news },
+//     { path: '/stars', component: stars }
+//   ] // (缩写) 相当于 routes: routes
+// })
+
+
+
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  // router
 }).$mount('#app')
